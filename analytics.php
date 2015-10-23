f3419adbe35ca0e447d432757f7527f09ede2215
Gabe Diaz <gdiaz@palmbeachletter.com>
2015-10-22 22:58:42 -0400

    removed quantcast code

--- analytics.php
+++ analytics.php
@@ -11,28 +11,3 @@
 
 </script>
 <?php //Google Analytics stop ?>
-
-<!-- Quantcast Tag -->
-<script type="text/javascript">
-var _qevents = _qevents || [];
-
-(function() {
-var elem = document.createElement('script');
-elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
-elem.async = true;
-elem.type = "text/javascript";
-var scpt = document.getElementsByTagName('script')[0];
-scpt.parentNode.insertBefore(elem, scpt);
-})();
-
-_qevents.push({
-qacct:"p-H0SXbyPs2PPdL"
-});
-</script>
-
-<noscript>
-<div style="display:none;">
-<img src="//pixel.quantserve.com/pixel/p-H0SXbyPs2PPdL.gif" border="0" height="1" width="1" alt="Quantcast"/>
-</div>
-</noscript>
-<!-- End Quantcast tag -->
